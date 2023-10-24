efa07bb6c7585a622e740ae7bacdaca15b727f1e
최종명 <lanto12@naver.com>
2023-10-24 10:54:59 +0900

    :wrench: Chore : 스웨거 의존성 주입, 설정 파일 추가

--- business/src/main/java/com/mzbr/business/user/controller/UserController.java
+++ business/src/main/java/com/mzbr/business/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.mzbr.business.user.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,9 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/users")
 public class UserController {
 	private final UserService userService;
-	
+
+	@GetMapping("/test")
+	public ResponseEntity<?> test() {
+		return ResponseEntity.ok("zzz");
+	}
 }
