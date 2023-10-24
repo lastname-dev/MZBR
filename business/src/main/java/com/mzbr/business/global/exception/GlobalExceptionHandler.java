9ed806768c787f4e8b3275f097278c2e7b73619c
최종명 <lanto12@naver.com>
2023-10-24 17:50:27 +0900

    ✨ Feat : 사용자 정의 Exception, ExceptionHandler 생성

---
+++ business/src/main/java/com/mzbr/business/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.mzbr.business.global.exception;
+
+import javax.naming.SizeLimitExceededException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.mzbr.business.global.exception.custom.AuthException;
+import com.mzbr.business.global.exception.custom.BadRequestException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(BadRequestException.class)
+	protected ResponseEntity<ErrorResponse> handleConflict(BadRequestException badRequestException) {
+		log.error("BadRequestException", badRequestException);
+		return handleExceptionInternal(badRequestException.getErrorCode());
+	}
+
+	@ExceptionHandler(AuthException.class)
+	protected ResponseEntity<ErrorResponse> handleConflict(AuthException authException) {
+		log.error("AuthException", authException);
+		return handleExceptionInternal(authException.getErrorCode());
+	}
+
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(Exception exception) {
+		log.error("Exception", exception);
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR,
+			exception.getMessage());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+	}
+
+	private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
+		return ResponseEntity
+			.status(errorCode.getHttpStatus().value())
+			.body(ErrorResponse.from(errorCode));
+	}
+}
