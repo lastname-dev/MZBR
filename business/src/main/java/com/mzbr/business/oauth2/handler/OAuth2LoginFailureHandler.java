a73f87b52988920f568cb6a85a9caf4fbc32322f
최종명 <lanto12@naver.com>
2023-10-27 09:38:44 +0900

    ✨ Feat : 로그인 성공, 실패 처리 핸들러

---
+++ business/src/main/java/com/mzbr/business/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -0,0 +1,30 @@
+package com.mzbr.business.oauth2.handler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import com.mzbr.business.global.exception.ErrorCode;
+import com.mzbr.business.global.exception.custom.AuthException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException exception) throws IOException, ServletException {
+		log.error(exception.getMessage());
+		throw new AuthException(ErrorCode.OAUTH_BAD_REQUEST);
+	}
+}
---
+++ business/src/main/java/com/mzbr/business/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,43 @@
+package com.mzbr.business.oauth2.handler;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import com.mzbr.business.global.exception.ErrorCode;
+import com.mzbr.business.global.exception.custom.BadRequestException;
+import com.mzbr.business.global.jwt.JwtService;
+import com.mzbr.business.member.entity.Member;
+import com.mzbr.business.member.repository.MemberRepository;
+import com.mzbr.business.oauth2.CustomOAuth2User;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+	private final JwtService jwtService;
+	private final MemberRepository memberRepository;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) {
+		CustomOAuth2User oAuth2User = (CustomOAuth2User)authentication.getPrincipal();
+
+		Member member = memberRepository.findByEmail(oAuth2User.getEmail())
+			.orElseThrow(() -> new BadRequestException(ErrorCode.USER_NOT_FOUND));
+		String accessToken = jwtService.createAccessToken(member.getId());
+		String refreshToken = jwtService.createRefreshToken();
+		jwtService.sendAccessToken(response, accessToken);
+		jwtService.sendRefreshToken(response, refreshToken);
+		jwtService.saveRefreshToken(refreshToken, member.getId());
+
+	}
+}
