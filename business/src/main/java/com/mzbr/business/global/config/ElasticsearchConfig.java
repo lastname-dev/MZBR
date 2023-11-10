67814ca025c9e3c964adeb9d5de91fb7bcd6cfab
최종명 <lanto12@naver.com>
2023-11-10 09:59:34 +0900

    ♻ Refactor : ElasitcSearch 보안 설정 추가

--- business/src/main/java/com/mzbr/business/global/config/ElasticsearchConfig.java
+++ business/src/main/java/com/mzbr/business/global/config/ElasticsearchConfig.java
@@ -2,9 +2,12 @@ package com.mzbr.business.global.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
@@ -12,11 +15,27 @@ public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 	private String ELASTIC_URL;
 	@Value(value = "${elasticsearch.port}")
 	private String ELASTIC_PORT;
+	@Value(value = "${elasticsearch.user_name}")
+	private String ELASTIC_USER_NAME;
+	@Value(value = "${elasticsearch.user_password}")
+	private String ELASTIC_USER_PASSWORD;
+
 	@Override
+	@Bean
 	public RestHighLevelClient elasticsearchClient() {
+		System.out.println(ELASTIC_USER_NAME + " " + ELASTIC_USER_PASSWORD);
+
 		ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-			.connectedTo(ELASTIC_URL+":"+ELASTIC_PORT)
+			.connectedTo(ELASTIC_URL + ":" + ELASTIC_PORT)
+			.withBasicAuth(ELASTIC_USER_NAME, ELASTIC_USER_PASSWORD)
 			.build();
 		return RestClients.create(clientConfiguration).rest();
+
 	}
+
+	@Bean
+	public ElasticsearchOperations elasticsearchOperations() {
+		return new ElasticsearchRestTemplate(elasticsearchClient());
+	}
+
 }
