3e3c59443ca0ff84dd93f3c6897a02dda9e1736e
최종명 <lanto12@naver.com>
2023-11-20 01:21:14 +0900

    ✏️ Fix : subscription 멤버 자료형 변경

--- business/src/main/java/com/mzbr/business/member/dto/SubScriptionId.java
+++ business/src/main/java/com/mzbr/business/member/dto/SubScriptionId.java
@@ -3,6 +3,6 @@ package com.mzbr.business.member.dto;
 import java.io.Serializable;
 
 public class SubScriptionId implements Serializable {
-	private int follower;
-	private int followee;
+	private long follower;
+	private long followee;
 }
